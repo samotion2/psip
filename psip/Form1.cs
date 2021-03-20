653c0137242e0e3a697fe0336c09d04a5618368c
unknown <uplnyrandom@gmail.com>
2021-03-20 09:32:37 +0100

    first commit

---
+++ psip/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PcapDotNet.Base;
+using PcapDotNet.Core;
+using PcapDotNet.Packets;
+using PcapDotNet.Packets.Arp;
+using PcapDotNet.Packets.Dns;
+using PcapDotNet.Packets.Ethernet;
+using PcapDotNet.Packets.Gre;
+using PcapDotNet.Packets.Http;
+using PcapDotNet.Packets.Icmp;
+using PcapDotNet.Packets.Igmp;
+using PcapDotNet.Packets.IpV4;
+using PcapDotNet.Packets.IpV6;
+using PcapDotNet.Packets.Transport;
+
+namespace psip {
+
+    public partial class Form1 : Form {
+        
+        public List<CAMTable> Data { get; set; }
+        public void updateUi(int[,] arr, int i) {
+
+            TextBox[] textboxarr = new TextBox[4] {textBox1, textBox2, textBox3, textBox4};
+
+            textboxarr[i].Invoke(new MethodInvoker(() => {
+                textboxarr[i].Text = "Ethernet: " + arr[i, 0] + "\r\nARP: " + arr[i, 1] + "\r\nIP: " + arr[i, 2] + "\r\nTCP: " + arr[i, 3] + "\r\nUDP: " + arr[i, 4] + "\r\nICMP: " + arr[i, 5] + "\r\nHTTP: " + arr[i, 6];
+            }));
+
+        }
+
+        public Form1() {
+            Data = CAMTable.InitTable();
+            InitializeComponent();
+        }
+
+        public List<CAMTable> getData() {
+            return this.Data;
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            // Retrieve the device list from the local machine
+            IList<LivePacketDevice> allDevices = LivePacketDevice.AllLocalMachine;
+           /* textBox1.Clear();
+            if (allDevices.Count == 0)
+            {
+                textBox1.AppendText("No interfaces found! Make sure WinPcap is installed." + Environment.NewLine);
+                return;
+            }*/
+
+            // Print the list
+           /* for (int i = 0; i != allDevices.Count; ++i)
+            {
+                LivePacketDevice device = allDevices[i];
+                textBox1.AppendText((i + 1) + ". " + device.Name);
+                if (device.Description != null)
+                    textBox1.AppendText(" (" + device.Description + ")" + Environment.NewLine);
+                else
+                    textBox1.AppendText(" (No description available)" + Environment.NewLine);
+            }*/
+
+
+            PacketDevice selectedDevice = allDevices[0];
+
+            using (PacketCommunicator communicator = selectedDevice.Open(100, PacketDeviceOpenAttributes.Promiscuous, 1000)) {
+                communicator.SendPacket(BuildDummyPacket());
+            }
+
+            Packet BuildDummyPacket() {
+                EthernetLayer ethernetLayer =
+                    new EthernetLayer {
+                        Source = new MacAddress("01:01:01:01:01:01"),
+                        Destination = new MacAddress("02:02:02:02:02:02"),
+                        EtherType = EthernetType.None, // Will be filled automatically.
+                    };
+
+                IpV4Layer ipV4Layer =
+                    new IpV4Layer {
+                        Source = new IpV4Address("1.2.3.4"),
+                        CurrentDestination = new IpV4Address("11.22.33.44"),
+                        Fragmentation = IpV4Fragmentation.None,
+                        HeaderChecksum = null, // Will be filled automatically.
+                        Identification = 123,
+                        Options = IpV4Options.None,
+                        Protocol = null, // Will be filled automatically.
+                        Ttl = 100,
+                        TypeOfService = 0,
+                    };
+
+                IcmpEchoLayer icmpLayer =
+                    new IcmpEchoLayer {
+                        Checksum = null, // Will be filled automatically.
+                        Identifier = 456,
+                        SequenceNumber = 800,
+                    };
+
+                PacketBuilder builder = new PacketBuilder(ethernetLayer, ipV4Layer, icmpLayer);
+
+                return builder.Build(DateTime.Now);
+            }
+
+
+        }
+
+        public void updateTable() {
+            var data = this.Data;
+            
+            dataGridView1.Invoke(new MethodInvoker(() => {
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = data;
+
+                //ocisluje riadky
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                    row.Cells["Id"].Value = (row.Index + 1).ToString();
+            }));
+            //aktualizuje data
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            for(int i = 0; i < 4; i ++) {
+                Controller.reset(i);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e) {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+
+            CAMTable.addToTable(this.Data, 100, new MacAddress("aa:aa:aa:aa:aa:aa"));
+            var data = this.Data;
+
+            //aktualizuje data
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = data;
+            
+            //ocisluje riadky
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+                row.Cells["Id"].Value = (row.Index + 1).ToString();
+            
+            //data.ForEach(item => Console.Write(item + ","));
+        }
+    }
+}
---
+++ psip/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using PcapDotNet.Core;
+using PcapDotNet.Packets;
+using PcapDotNet.Packets.IpV4;
+using PcapDotNet.Packets.Transport;
+using PcapDotNet.Packets.Ethernet;
+using System.Diagnostics;
+
+namespace psip {
+    public class ThreadWork {
+        public static void DoWork(PacketCommunicator communicator, PacketCommunicator communicator1, Controller con, int i) {
+            Packet packet;
+            do {
+                PacketCommunicatorReceiveResult result = communicator.ReceivePacket(out packet);
+                switch (result) {
+                    case PacketCommunicatorReceiveResult.Timeout:
+                        // Timeout elapsed
+                        continue;
+                    case PacketCommunicatorReceiveResult.Ok:
+                        con.updateStats(packet, i);
+                        con.updateTable(i, packet.Ethernet.Source);
+                        //Console.WriteLine(packet.Timestamp.ToString("yyyy-MM-dd hh:mm:ss.fff") + " length:" + packet.Length);
+                        //Stopwatch sw = new Stopwatch();
+                        //->>>>>>>pomaly retos fix trba tu->>>>>>>>>>>>>
+                        //sw.Start();
+                        if (con.isInTable(i, packet.Ethernet.Destination)) {
+                            //statistiky pre druhy kidos
+                            communicator1.SendPacket(packet);
+                            con.updateStats(packet, i + 1);
+                            break;
+                        }
+                        //sw.Stop();
+                        //Console.WriteLine("Elapsed={0}", sw.Elapsed);
+
+                        //Console.WriteLine("SRC_MAC: " + packet.Ethernet.Source);
+                        break;
+                    default:
+                        throw new InvalidOperationException("The result " + result + " shoudl never be reached here");
+                }
+            } while (true);   
+        }
+    }
+
+    public class Controller {
+        static Form1 form;
+        static int[,] arr = new int[4,7];
+        public Controller(Form1 form1) {
+            form = form1;
+        }
+        public void updateStats(Packet packet, int i) {
+            if (packet.IsValid) {
+                if (packet.DataLink.Kind == DataLinkKind.Ethernet)
+                    arr[i, 0] += 1;
+                if (packet.Ethernet.EtherType == EthernetType.Arp)
+                    arr[i, 1] += 1;
+                if (packet.Ethernet.EtherType == EthernetType.IpV4)
+                    arr[i, 2] += 1;
+                if (packet.Ethernet.IpV4.Protocol == IpV4Protocol.Tcp)
+                    arr[i, 3] += 1;
+                if (packet.Ethernet.IpV4.Protocol == IpV4Protocol.Udp)
+                    arr[i, 4] += 1;
+                if (packet.Ethernet.IpV4.Protocol == IpV4Protocol.InternetControlMessageProtocol)
+                    arr[i, 5] += 1;
+                if (packet.Ethernet.IpV4.Protocol == IpV4Protocol.Tcp && (packet.Ethernet.IpV4.Tcp.SourcePort == 80 || packet.Ethernet.IpV4.Tcp.DestinationPort == 80))
+                    arr[i, 6] += 1;
+
+                form.updateUi(arr, i);
+            }
+        }
+        public static void reset(int x) {
+            for(int i = 0; i < 7; i++) {
+                arr[x, i] = 0;
+            }
+            form.updateUi(arr, x);
+        }
+        public void updateTable(int i, MacAddress mac) {
+            //Console.WriteLine(mac);
+            CAMTable.addToTable(form.getData(), i, mac);
+            form.updateTable();
+        }
+        public bool isInTable(int i, MacAddress mac) {
+            var temp = form.getData();
+            int count = 0;
+
+            //Console.WriteLine(temp + ",");
+            //ak sa mac nenachadza v tabulke vrati true
+            foreach (var item in temp) {
+                if (item.Mac.Equals(mac))
+                    count++;
+            }
+            if (count == 0) {
+                return true;
+            }
+
+            
+            //ak sa max nachadza v tabulke a zaroven sa nenachadza na rovnakom porte na ktory dosiel packet, vrati true
+            foreach (var item in temp) {
+                if (item.Mac.Equals(mac) && (item.Port != i)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    public class CAMTable {
+        public string Id { get; set; }
+        public int Port { get; set; }
+        public MacAddress Mac { get; set; }
+        public string Timer { get; set; }
+
+        public static List<CAMTable> InitTable() {
+            var list = new List<CAMTable>();
+            list.Add(new CAMTable() { Id = null, Port = 100, Mac = new MacAddress("aa:aa:aa:aa:aa:aa"), Timer = "1" });
+            return list;
+
+        }
+
+        public static List<CAMTable> addToTable(List<CAMTable> list1, int i, MacAddress mac) {
+            CAMTable list = new CAMTable() { Id = "", Port = i, Mac = mac, Timer = "1" };
+
+
+            //TU PUSTIT KIDKOSA
+            //ak sa src_mac uz nachadza v liste neprida sa
+            try {
+                foreach (var item in list1) {
+                    if (item.Mac == mac)
+                        return list1;
+                }
+            } catch {}
+            
+            list1.Add(list);
+            return list1;
+        }
+    }
+
+    static class Program {
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            IList<LivePacketDevice> allDevices = LivePacketDevice.AllLocalMachine;
+
+            PacketCommunicator communicator = allDevices[0].Open(65536, PacketDeviceOpenAttributes.Promiscuous | PacketDeviceOpenAttributes.NoCaptureLocal, 1);
+            PacketCommunicator communicator1 = allDevices[1].Open(65536, PacketDeviceOpenAttributes.Promiscuous | PacketDeviceOpenAttributes.NoCaptureLocal, 1);
+
+            Form1 form = new Form1();
+            Controller con = new Controller(form);
+
+            //z 1 na 2
+            Thread thread = new Thread(() => ThreadWork.DoWork(communicator1, communicator, con, 0));
+            thread.Start();
+            Thread thread2 = new Thread(() => ThreadWork.DoWork(communicator, communicator1, con, 2));
+            thread2.Start();
+
+            Application.Run(form);
+        }
+    }
+}
